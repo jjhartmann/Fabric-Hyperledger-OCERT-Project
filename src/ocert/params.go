9ece443ccf30df78e953e5bcf48ee0c381070a31
skw1234567890 <charlie.shikewei@gmail.com>
2017-11-13 20:00:54 -0500

    add generateParams

---
+++ src/ocert/params.go
@@ -0,0 +1,21 @@
+package ocert
+
+import (
+	"github.com/Nik-U/pbc"
+)
+
+/*
+ * Randomly generate a paired group and the corresponding
+ * generator of each group
+ */
+func GenerateSharedParams() *SharedParams {
+	sharedParams := new(SharedParams)
+	params := pbc.GenerateF(160)
+	pairing := params.NewPairing()
+	g1 := pairing.NewG1().Rand()
+	g2 := pairing.NewG2().Rand()
+	sharedParams.Params = params.String()
+	sharedParams.G1 = g1.Bytes()
+	sharedParams.G2 = g2.Bytes()
+	return sharedParams
+}
--- src/ocert/test_structure_preserving.go
+++ src/ocert/test_structure_preserving.go
@@ -10,26 +10,23 @@ import (
  * Run a single test
  */
 func Stest() bool {
-	fmt.Println("Stest")
-	sharedParams := new(SharedParams)
-	params := pbc.GenerateF(160)
-	pairing := params.NewPairing()
-	g1 := pairing.NewG1().Rand()
-	g2 := pairing.NewG2().Rand()
-	sharedParams.Params = params.String()
-	sharedParams.G1 = g1.Bytes()
-	sharedParams.G2 = g2.Bytes()
-
+	sharedParams := GenerateSharedParams()
 	VK, SK := SKeyGen(sharedParams)
 
 	P := new(Pseudonym)
+	pairing, _ := pbc.NewPairingFromString(sharedParams.Params)
 	P.C = pairing.NewG1().Rand().Bytes()
 	P.D = pairing.NewG1().Rand().Bytes()
 	PKc := new(ClientPublicKey)
 	PKc.PK = pairing.NewG2().Rand().Bytes()
 	ecert := SSign(sharedParams, SK, P, PKc)
 
-	return SVerify(sharedParams, VK, P, PKc, ecert)
+	if SVerify(sharedParams, VK, P, PKc, ecert) {
+		fmt.Println("Stest passes")
+		return true
+	} else {
+		return false
+	}
 }
 
 /*
