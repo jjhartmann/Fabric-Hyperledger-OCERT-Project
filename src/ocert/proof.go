a3511bad51f2aeda4ee653394958fc6a18bd994b
Jeremy J. Hartmann <jeremy.hartmann@gmail.com>
2017-11-24 00:58:35 -0500

    Add commit key and common reference string generator

--- src/ocert/proof.go
+++ src/ocert/proof.go
@@ -4,8 +4,7 @@
 
 package ocert
 
-import (
-)
+import "github.com/Nik-U/pbc"
 
 /*
  * Set up the proof of knowledge, called by the client. It takes a system
@@ -37,4 +36,52 @@ func Prove(sharedParams *SharedParams, pi *ProofOfKnowledge, consts *ProofConsta
 	// TODO validate eq5
 
 	return false
+}
+
+/*
+ * Create Common refernce string sigma.
+ * sigma = (u1, u2, v1, v2)
+ * u1 and u1 are in B1
+ * v1 and v2 are in B2
+ *
+ * u1 = (O, P)
+ * u2 = t * u1
+ */
+func CreateCommonReferenceString(sharedParams *SharedParams) *Sigma {
+	pairing, _ := pbc.NewPairingFromString(sharedParams.Params)
+	g1 := pairing.NewG1().SetBytes(sharedParams.G1)
+	g2 := pairing.NewG2().SetBytes(sharedParams.G2)
+	sigma := new(Sigma)
+
+	// Create commit keys for u1 and u2 on G1
+	alpha := pairing.NewZr().Rand()
+
+	p1 := g1.Bytes()
+	q1 := pairing.NewG1().MulZn(g1, alpha)
+
+	t := pairing.NewZr().Rand()
+	p2 := pairing.NewG1().MulZn(g1, t)
+	q2 := pairing.NewG1().MulZn(q1, t)
+
+	sigma.U = []CommitmentKey{
+		CommitmentKey{p1, q1.Bytes()},
+		CommitmentKey{p2.Bytes(), q2.Bytes()},
+	}
+
+	// Create commit keys v1 and v2 on G2
+	alpha2 := pairing.NewZr().Rand()
+
+	p12 := g2.Bytes()
+	q12 := pairing.NewG1().MulZn(g2, alpha2)
+
+	t2 := pairing.NewZr().Rand()
+	p22 := pairing.NewG1().MulZn(g2, t2)
+	q22 := pairing.NewG1().MulZn(q1, t2)
+
+	sigma.V = []CommitmentKey{
+		CommitmentKey{p12, q12.Bytes()},
+		CommitmentKey{p22.Bytes(), q22.Bytes()},
+	}
+
+	return sigma
 }
