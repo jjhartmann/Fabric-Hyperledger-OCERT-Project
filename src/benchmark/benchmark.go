e0cd502aa8f4cb61d7bac7cba13061fbc32e4e0d
skw1234567890 <charlie.shikewei@gmail.com>
2017-11-27 15:27:51 -0500

    add benchmarkcc.go

--- src/benchmark/benchmark.go
+++ src/benchmark/benchmark.go
@@ -1,3 +1,7 @@
+/*
+ * Used to benchmark ocert_scheme
+ */
+
 package main
 
 import (
---
+++ src/benchmark/benchmarkcc.go
@@ -0,0 +1,59 @@
+/*
+ * Used to benchmark ocert chaincode
+ */
+
+package main
+
+import (
+	"os/exec"
+	"fmt"
+	"ocert"
+)
+
+func parseOut(out []byte) []byte {
+	str := string(out)
+	str = str[14:]
+	return []byte(str)
+}
+
+func setup() {
+	installCmd := "peer chaincode install -p chaincodedev/chaincode/ocert -n myccc -v 0"
+	_, err := exec.Command("sh","-c", installCmd).Output()
+	if err != nil {
+		fmt.Println(err)
+		panic(err.Error())
+	}
+
+	instantiateCmd := "peer chaincode instantiate -n mycc -v 0 -c '{\"Args\":[]}' -C myc"
+	_, err = exec.Command("sh","-c", instantiateCmd).Output()
+	if err != nil {
+		fmt.Println(err)
+		panic(err.Error())
+	}
+}
+
+func sharedParams() *ocert.SharedParams {
+	queryCmd := "peer chaincode query -n mycc -c '{\"Args\":[\"sharedParams\"]}' -C myc"
+	out, err := exec.Command("sh","-c", queryCmd).Output()
+
+	if err != nil {
+		fmt.Println(err)
+		panic(err.Error())
+	}
+	
+	sharedParams := new(ocert.SharedParams)
+	err = sharedParams.SetBytes(parseOut(out))
+
+	if err != nil {
+		fmt.Println(err)
+		panic(err.Error())
+	}
+
+	return sharedParams
+}
+
+
+func main () {
+	setup()
+	fmt.Println(sharedParams())
+}
