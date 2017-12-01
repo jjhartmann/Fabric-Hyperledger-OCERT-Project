61b896c55741c2eeed90688592ec8b75052265ea
skw1234567890 <charlie.shikewei@gmail.com>
2017-12-01 11:10:34 -0500

    update local benchmark

--- src/benchmark/benchmark.go
+++ src/benchmark/benchmark.go
@@ -7,6 +7,7 @@ package main
 import (
 	"fmt"
 	"ocert"
+	"github.com/Nik-U/pbc"
 )
 
 type DB struct {
@@ -31,20 +32,47 @@ func main() {
 	db := new(DB)
 	db.DB = make(map[string][]byte)
 
-	// TODO delete
-	var argPut []string
-	argPut = append(argPut, "key1")
-	argPut = append(argPut, "value1")
-
-	ocert.Put(db, argPut)
-
-	var argGet []string
-	argGet = append(argGet, "key1")
-	val, err := ocert.Get(db, argGet)
-
-	if err == nil {
-		fmt.Println(string(val))
+	// Benchmark starts here
+	setupArgs := [][]byte{}
+	_, err := ocert.Setup(db, setupArgs)
+	if err != nil {
+		fmt.Println(err)
+		panic(err.Error())
 	}
 
-	// Benchmark starts here
+	sharedParamsBytes, err := ocert.GetSharedParams(db, setupArgs)
+	sharedParams := new(ocert.SharedParams)
+	err = sharedParams.SetBytes(sharedParamsBytes)
+	if err != nil {
+		fmt.Println(err)
+		panic(err.Error())
+	}
+	fmt.Println(sharedParams)
+	pairing, _ := pbc.NewPairingFromString(sharedParams.Params)
+
+	IDc := new(ocert.ClientID)
+	IDc.ID = pairing.NewG1().Rand().Bytes()
+	PKc := new(ocert.ClientPublicKey)
+	PKc.PK = pairing.NewG1().Rand().Bytes()
+	ecertRequest := new(ocert.GenECertRequest)
+	ecertRequest.IDc = IDc.ID
+	ecertRequest.PKc = PKc.PK
+	ecertRequestBytes, err := ecertRequest.Bytes()
+	if err != nil {
+		fmt.Println(err)
+		panic(err.Error())
+	}
+	ecertArgs := [][]byte{ecertRequestBytes}
+
+	ecertReplyBytes, err := ocert.GenECert(db, ecertArgs)
+	if err != nil {
+		fmt.Println(err)
+		panic(err.Error())
+	}
+	ecertReply := new(ocert.GenECertReply)
+	err = ecertReply.SetBytes(ecertReplyBytes)
+	if err != nil {
+		fmt.Println(err)
+		panic(err.Error())
+	}
 }
