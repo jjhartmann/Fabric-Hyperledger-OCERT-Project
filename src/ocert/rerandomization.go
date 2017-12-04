a9ced7209393ac54c01ea9ee2b88368ef09e8719
Dhara <dharvi.verma@gmail.com>
2017-12-03 21:01:06 -0500

    Passed PKa in ERerand, check if this works.

--- src/ocert/rerandomization.go
+++ src/ocert/rerandomization.go
@@ -65,6 +65,11 @@ func EEnc(sharedParams *SharedParams, PKa *AuditorPublicKey, id *ClientID) *Pseu
  */
 func EDec(sharedParams *SharedParams, SKa *AuditorSecretKey, P *Pseudonym) *ClientID {
 	id := new(ClientID)
+
+	pairing, _ := pbc.NewPairingFromString(sharedParams.Params)
+
+
+
 	return id
 }
 
@@ -74,7 +79,7 @@ func EDec(sharedParams *SharedParams, SKa *AuditorSecretKey, P *Pseudonym) *Clie
  * P' = (C', D'), where P' is also in G1 * G1.
  */
 // TODO This function may also return the extra information used in validation
-func ERerand(sharedParams *SharedParams, P *Pseudonym) *Pseudonym {
+func ERerand(sharedParams *SharedParams,PKa *AuditorPublicKey, P *Pseudonym) *Pseudonym {
 	// TODO rerandomize P
 	pairing, _ := pbc.NewPairingFromString(sharedParams.Params)
 	g1 := pairing.NewG1().SetBytes(sharedParams.G1)
@@ -93,6 +98,7 @@ func ERerand(sharedParams *SharedParams, P *Pseudonym) *Pseudonym {
 	Cprime:= pairing.NewG1().Add(C,tempC)
 
   //To find Dprime, using D from Pseudonym & product of rprime & Public Key
+	PKa := new(AuditorPublicKey)
 	PK:=pairing.NewG1().SetBytes(PKa.PK)
   tempD:=pairing.NewG1().MulZn(PK,rprime)
 	Dprime:=pairing.NewG1().Add(D,tempD)
