7a6c6c04492b37fe3472d97aa81b3ebaa35ca954
skw1234567890 <charlie.shikewei@gmail.com>
2017-12-06 12:17:48 -0500

    clean up certificate.go as we don't use X.509 certificate in our implementation

--- src/ocert/certificate.go
+++ src/ocert/certificate.go
@@ -7,14 +7,6 @@ package ocert
 
 import (
  	"fmt"
-	"math/big"
-	"time"
-	// "crypto"
-	"crypto/rsa"
-	"crypto/rand"
-	// "crypto/sha256"
-	"crypto/x509"
-	"crypto/x509/pkix"
 )
 
 /*
@@ -29,52 +21,4 @@ func OCertSingedBytes(PKc *ClientPublicKey, P *Pseudonym) ([]byte, error) {
 	}
 	result = append(result[:], Pbytes[:]...)
 	return result, nil
-}
-
-// TODO
-/*
- * Sign PKc|P by RSA and generate the X.509 certificate that contains the
- * signature, which is the ocert
- */
-func GenOCertHelper(
-	PKc *ClientPublicKey, 
-	P *Pseudonym, 
-	privateKey *rsa.PrivateKey,
-	serialNumber *big.Int) ([]byte, error) {
-
-	msg, err := OCertSingedBytes(PKc, P)
-	if err != nil {
-		return nil, err
-	}
-
-	notBefore := time.Now()
-	notAfter := notBefore.Add(365 * 24 * time.Hour)
-
-	template := x509.Certificate{
-		SignatureAlgorithm: x509.SHA256WithRSA,
-
-		PublicKeyAlgorithm: x509.RSA,
-		PublicKey: &privateKey.PublicKey,
-
-  		SerialNumber: serialNumber,
-  		
-  		Issuer: pkix.Name {
-  			CommonName: "Test Ocert CA",
-  		},
-  		Subject: pkix.Name {
-  			CommonName: "Test client",
-  		},
-
-  		NotBefore: notBefore,
-  		NotAfter: notAfter,
-
-  		SubjectKeyId: msg,
-  	}
-
-  	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &privateKey.PublicKey, privateKey)
-	if err != nil {
-		return nil, err
-	}
-
-	return derBytes, nil
 }
