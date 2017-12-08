ca63289f58ef528df7fd55d1ae8eab2080b11859
skw1234567890 <charlie.shikewei@gmail.com>
2017-12-08 16:09:29 -0500

    add benchmark for security parameter 320

--- benchmark/benchmarkcc.go
+++ benchmark/benchmarkcc.go
@@ -255,17 +255,17 @@ var genECertLog *os.File
 
 func main () {
     var err error
-    genOCertLog, err = os.Create("/data/genOCertLog160.txt")
+    genOCertLog, err = os.Create("/data/genOCertLog320.txt")
     if err != nil {
         fmt.Println(err)
         panic(err.Error())
     }
-    genProofLog, err = os.Create("/data/genProofLog160.txt")
+    genProofLog, err = os.Create("/data/genProofLog320.txt")
     if err != nil {
         fmt.Println(err)
         panic(err.Error())
     }
-    genECertLog, err = os.Create("/data/genECertLog160.txt")
+    genECertLog, err = os.Create("/data/genECertLog320.txt")
     if err != nil {
         fmt.Println(err)
         panic(err.Error())
